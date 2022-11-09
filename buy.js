0e7a3d00b10f54d873734263fccbbf5532ed1ee6
Normasj <normasj.rp@gmail.com>
2022-11-09 17:09:28 -0300

    ticket commit

---
+++ buy.js
@@ -0,0 +1,61 @@
+
+
+        cuadro.id === 'cuadro1' && '1' 
+        cuadro.id === 'cuadro2' && '2'
+        cuadro.id === 'cuadro3' && '3'
+        if (nombre==""){
+            alert("El nombre es obligatorio")}
+            if (apellido==""){
+                alert("El apellido es obligatorio")}
+                if (email==""){
+                    alert("El email es obligatorio")}
+                    if (cantidad==""){
+                        alert("Este dato es obligatorio")}
+  
+                       
+ function btnBorrar{
+   container.input-= ''
+   }               
+  var btnEnviar = document.getElementById("btnEnviar");     
+var cantidad
+var precio
+var cantidad, precio, descuento, compra, total
+
+function prime(precio, cantidad){ 
+    var init; 
+    init = (precio*cantidad); 
+    return init;
+   }
+
+function condesci(init, compra){ 
+    var condesc; 
+    condesc = (init-compra); 
+    return condesc; 
+}
+
+{if  tipo.id === 'cuadro1' 
+func (prime);
+compra = init*0.80;
+func (condesci);
+}
+
+{
+if  tipo.id === 'cuadro2'
+func (prime);
+compra = init*0.50;
+func (condesci);
+}
+
+
+else {
+ func (prime);
+compra = init*0.15;
+func (condesci);
+}
+
+{ 
+localStorage.setItem(condesc);
+localStorage.getItem('condesc');
+get 'condesc' = document.getElementById('total').value;
+}
+</script
